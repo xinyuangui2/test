db54263e1887e0979ad298329e000ce23de7dd85
xinyuangui <guixinyuanwh@163.com>
2018-11-28 18:37:55 -0500

    add thread wait

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,8 +6,9 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!" );
+		Thread.sleep(20000);
     }
 }
